b088c63a69b7e70470af331fefdd6ff9e28f1a3d
Andrew Elbert <elbeab19@wfu.edu>
2025-01-26 18:59:40 -0500

    Initial Commit

---
+++ BodyPositiveZumba/BodyPositiveZumbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyPositiveZumbaApp.swift
+//  BodyPositiveZumba
+//
+//  Created by Andrew Elbert on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BodyPositiveZumbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
