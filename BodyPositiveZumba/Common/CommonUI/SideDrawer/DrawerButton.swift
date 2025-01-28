3a4025691462f74defe4a2e738a2ec8861db4bc4
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-01-27 20:13:28 -0500

    [ae-5] designed the side drawer + implemented constants where needed

--- BodyPositiveZumba/Common/CommonUI/SideDrawer/DrawerButton.swift
+++ BodyPositiveZumba/Common/CommonUI/SideDrawer/DrawerButton.swift
@@ -9,15 +9,48 @@ import SwiftUI
 
 struct DrawerButton: View {
     let title: String
+    let icon: String
     let action: () -> Void
 
     var body: some View {
         Button(action: action) {
-            Text(title)
-                .font(.headline)
-                .foregroundColor(.black)
-                .padding(.vertical, 10)
+            HStack {
+                Image(systemName: icon)
+                    .resizable()
+                    .frame(
+                        width: Constants.SideDrawer.iconSize,
+                        height: Constants.SideDrawer.iconSize)
+                    .foregroundColor(Color(white: 0.99))
+
+                Text(title)
+                    .font(.system(
+                        size: Constants.SideDrawer.fontSize,
+                        weight: .bold)
+                    )
+                    .foregroundColor(Color(white: 0.99))
+            }
+            .padding(.vertical, 12)
+            .padding(.leading, 20)
+            .padding(.trailing, 20)
+            .background(
+                RoundedRectangle(cornerRadius: Constants.SideDrawer.cornerRadius)
+                    .fill(LinearGradient(
+                        gradient: Gradient(colors: [
+                            Constants.SideDrawer.buttonStartColor,
+                            Constants.SideDrawer.buttonEndColor
+                        ]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    ))
+            )
+            .cornerRadius(Constants.SideDrawer.cornerRadius)
+            .overlay(
+                RoundedRectangle(cornerRadius: Constants.SideDrawer.cornerRadius)
+                    .stroke(Color.black, lineWidth: 1)
+            )
+            .padding(.horizontal, 10)
+            .scaleEffect(1.05)
         }
-        .padding(.leading, 20)
+        .hoverEffect(.highlight)
     }
 }
