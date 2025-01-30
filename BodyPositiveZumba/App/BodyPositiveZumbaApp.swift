dcc5baf4648b6ffbec397f672795999157ef0238
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-01-30 18:17:58 -0500

    [11] initial app load refactor

--- BodyPositiveZumba/App/BodyPositiveZumbaApp.swift
+++ BodyPositiveZumba/App/BodyPositiveZumbaApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct BodyPositiveZumbaApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                InitialAppLoadView()
-            }
+            InitialLoadRootCoordinatorView()
         }
     }
 }
