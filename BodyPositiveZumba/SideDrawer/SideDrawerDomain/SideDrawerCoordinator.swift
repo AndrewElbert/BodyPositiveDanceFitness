7e13d94c1fdcf0a32ccc9e778dca348569a6d1a8
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-01-30 18:16:52 -0500

    [11] set up basic coordinator stuff

---
+++ BodyPositiveZumba/SideDrawer/SideDrawerDomain/SideDrawerCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  SideDrawerCoordinator.swift
+//  BodyPositiveZumba
+//
+//  Created by Andrew Elbert on 1/29/25.
+//
+
+import Foundation
+
+protocol SideDrawerCoordinator: ScreenCoordinator {
+    
+    func sideDrawer_viewJoinNow()
+    func sideDrawer_viewClasses()
+    func sideDrawer_viewNewcomers()
+    func sideDrawer_viewAbout()
+    func sideDrawer_viewContact()
+    func sideDrawer_viewSignIn()
+    func sideDrawer_viewSubscription()
+}
