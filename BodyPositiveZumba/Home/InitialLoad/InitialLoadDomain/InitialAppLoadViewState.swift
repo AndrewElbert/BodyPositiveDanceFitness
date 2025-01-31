06f66d6ebc61c7543bc3323eff43bd10777dffe2
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-01-31 15:49:00 -0500

    [16] updated initial load

--- BodyPositiveZumba/Home/InitialLoad/InitialLoadDomain/InitialAppLoadViewState.swift
+++ BodyPositiveZumba/Home/InitialLoad/InitialLoadDomain/InitialAppLoadViewState.swift
@@ -12,8 +12,8 @@ struct InitialAppLoadViewState {
     var showHomeScreen: Bool = false
     var progress: CGFloat = 0
     var fadeInProgress: Double = 0
-    let barLoadDuration: Double = Double.random(in: 0.88...2.22)
-    let barLoadPause: Double = 2.0
+    let barLoadDuration: Double = Double.random(in: 0.77...2.0)
+    let barLoadPause: Double = 2.5
     let colorChangeDelay: Double = 0.3
     var textColor: Color = .blue
     var barColorStart: Color = .cyan
