01dc1470118a4475fa77f1cc6a2819f172f5d20a
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-01 16:51:42 -0500

    [19] massage view is state only driven!

--- BodyPositiveZumba/Massage/MassageDirector/MassageUI.swift
+++ BodyPositiveZumba/Massage/MassageDirector/MassageUI.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MassageUI {
     static func massagePage(
     ) -> some View {
-        MassageView(
+        MassageScreen(
             viewModel: MassageViewModel(
             )
         )
--- BodyPositiveZumba/Massage/MassageUI/MassageView.swift
+++ BodyPositiveZumba/Massage/MassageUI/MassageView.swift
@@ -7,31 +7,46 @@
 
 import SwiftUI
 
-struct MassageView: View {
+struct MassageView: View, ActionableView {
     
+    enum Action {
+        case updateUrl
+    }
+
+    var onAction: ((Action) -> Void)?
     @Environment(\.dismiss) var dismiss
-    @ObservedObject var viewModel: MassageViewModel
+    @Binding var viewState: MassageViewState
+
+    public init(
+        viewState: Binding<MassageViewState>,
+        onAction: ((Action) -> Void)? = nil
+    ) {
+        self._viewState = viewState
+        self.onAction = onAction
+    }
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
             VStack {
-                Text(viewModel.viewState.pageTitle)
+                Text(viewState.pageTitle)
                     .font(.system(size: 34, weight: .bold, design: .serif))
                     .multilineTextAlignment(.center)
                     .foregroundColor(Constants.Colors.darkerCyan)
                     .padding()
 
-                Text(viewModel.viewState.pageBio)
+                Text(viewState.pageBio)
                     .font(.system(size: 16, design: .serif))
                     .foregroundColor(.black.opacity(0.7))
                     .multilineTextAlignment(.center)
                     .padding(.horizontal)
                     .padding(.bottom, 16)
 
-                InfiniteCarouselView(items: viewModel.viewState.cards,
-                                       currentIndex: $viewModel.viewState.currentIndex,
-                                       spacing: 10,
-                                       sideSpacing: 40) { card, isCurrentCard in
+                InfiniteCarouselView(
+                    items: viewState.cards,
+                    currentIndex: $viewState.currentIndex,
+                    spacing: 10,
+                    sideSpacing: 40
+                ) { card, isCurrentCard in
                     SwipableCardView(card: card)
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
@@ -43,18 +58,24 @@ struct MassageView: View {
 
                 VStack(spacing: 8) {
                     Text("Swipe")
-                        .font(.system(size: 16, weight: .medium, design: .serif))
+                        .font(
+                            .system(
+                                size: 16,
+                                weight: .medium,
+                                design: .serif
+                            )
+                        )
                         .foregroundColor(.gray)
 
-                    SwipeAnimationView(isAnimating: $viewModel.viewState.isAnimating)
+                    SwipeAnimationView(isAnimating: $viewState.isAnimating)
                 }
                 .padding(.top, 60)
                 .padding(.bottom, 15)
 
                 Button(action: {
-                    viewModel.updateBookingURL()
+                    onAction?(.updateUrl)
                 }) {
-                    Text("Book Today!")
+                    Text(Constants.Massage.buttonText)
                         .font(.system(size: 18, weight: .bold))
                         .foregroundColor(.black)
                         .padding(.horizontal, 20)
@@ -81,7 +102,7 @@ struct MassageView: View {
                 dismiss()
             })
         }
-        .sheet(item: $viewModel.viewState.bookingURL) { booking in
+        .sheet(item: $viewState.bookingURL) { booking in
             WebView(url: booking.url)
                 .overlay(
                     CloseButton(dismiss: {
