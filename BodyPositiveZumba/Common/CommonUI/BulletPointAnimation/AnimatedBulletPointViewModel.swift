f5188a31b4765f00c8235640ce0e056f44ca249f
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-02 17:23:46 -0500

    ae/23 - refactored animated bullet points

---
+++ BodyPositiveZumba/Common/CommonUI/BulletPointAnimation/AnimatedBulletPointViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AnimatedBulletPointViewModel.swift
+//  BodyPositiveZumba
+//
+//  Created by Andrew Elbert on 2/2/25.
+//
+
+import SwiftUI
+
+@MainActor
+class AnimatedBulletPointViewModel: ObservableObject {
+    
+    @Binding var viewState: AnimatedBulletPointViewState
+    
+    init(viewState: Binding<AnimatedBulletPointViewState>) {
+        self._viewState = viewState
+    }
+    
+    func startAnimation() {
+        withAnimation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true)) {
+            viewState.isAnimating.toggle()
+            viewState.sparkleOpacity = 1
+        }
+    }
+}
