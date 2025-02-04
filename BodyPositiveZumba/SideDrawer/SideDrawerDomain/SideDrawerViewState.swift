4e1152a57e10ec1a5f940a4fce07ecaa9580ab84
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-04 16:05:52 -0500

    refactored the sideDrawerButton

--- BodyPositiveZumba/SideDrawer/SideDrawerDomain/SideDrawerViewState.swift
+++ BodyPositiveZumba/SideDrawer/SideDrawerDomain/SideDrawerViewState.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct SideDrawerViewState {
-
+    
     var isMenuOpen: Bool = false
     var showDimBackground: Bool = false
-    var dragOffset = CGSize.zero
+    var dragOffset: CGSize = .zero
     var newsLetterIsPressed: Bool = false
+    
+    var drawerButtons: [DrawerButtonViewState] = [
+        DrawerButtonViewState(
+            title: Constants.SideDrawer.joinNowText,
+            icon: Constants.SideDrawer.joinNowImage,
+            type: .joinNow
+        ),
+        DrawerButtonViewState(
+            title: Constants.SideDrawer.classesText,
+            icon: Constants.SideDrawer.classesImage,
+            type: .classes
+        ),
+        DrawerButtonViewState(
+            title: Constants.SideDrawer.faqText,
+            icon: Constants.SideDrawer.faqImage,
+            type: .faq
+        ),
+        DrawerButtonViewState(
+            title: Constants.SideDrawer.aboutText,
+            icon: Constants.SideDrawer.aboutImage,
+            type: .about
+        ),
+        DrawerButtonViewState(
+            title: Constants.SideDrawer.massageText,
+            icon: Constants.SideDrawer.massageImage,
+            type: .massage
+        ),
+        DrawerButtonViewState(
+            title: Constants.SideDrawer.spaceRentalText,
+            icon: Constants.SideDrawer.spaceRentalImage,
+            type: .spaceRental
+        )
+    ]
 }
+
