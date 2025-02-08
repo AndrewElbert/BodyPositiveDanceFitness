386c1c42ebcee2d61990cd6b105bf557cfcefacd
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-07 21:12:42 -0500

    [29] updated ui

--- BodyPositiveZumba/Common/Constants.swift
+++ BodyPositiveZumba/Common/Constants.swift
@@ -60,6 +60,38 @@ struct Constants {
 
     }
 
+    // MARK: Contact
+
+    enum Contact {
+
+        static let contactRows: [ContactRowData] = [
+            ContactRowData(
+                title: "Message Us!",
+                icon: "message.fill",
+                text: "Chat",
+                action: "https://www.bodypositivedancefitness.com/contact-1"
+            ),
+            ContactRowData(
+                title: "Call Us!",
+                icon: "phone.fill",
+                text: "207-536-4617",
+                action: "tel://207-536-4617"
+            ),
+            ContactRowData(
+                title: "Email Us!",
+                icon: "envelope.fill",
+                text: "dancing@bodypositivedancefitness.com",
+                action: "mailto:dancing@bodypositivedancefitness.com"
+            ),
+            ContactRowData(
+                title: "Visit Us!",
+                icon: "map.fill",
+                text: "1100 Brighton Avenue Pine Tree Shopping Center Portland, Maine",
+                action: "http://maps.apple.com/?q=123+Main+Street"
+            )
+        ]
+    }
+
     // MARK: FAQ
 
     enum FAQ {
