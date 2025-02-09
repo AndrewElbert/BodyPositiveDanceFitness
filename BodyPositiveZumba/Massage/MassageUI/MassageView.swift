deb7ce0472c18abac758a33047b6c11dc0651efc
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-09 13:58:40 -0500

    [33] navigation adjustments

--- BodyPositiveZumba/Massage/MassageUI/MassageView.swift
+++ BodyPositiveZumba/Massage/MassageUI/MassageView.swift
@@ -5,10 +5,10 @@
 //  Created by Andrew Elbert on 1/28/25.
 //
 
+
 import SwiftUI
 
 struct MassageView: View, ActionableView {
-
     enum Action {
         case updateUrl
     }
@@ -17,7 +17,7 @@ struct MassageView: View, ActionableView {
     @Environment(\.dismiss) var dismiss
     @Environment(\.colorScheme) private var colorScheme
     @Binding var viewState: MassageViewState
-
+    
     private var adaptiveTextColor: Color {
         colorScheme == .dark ? Color.white.opacity(0.9) : Color.black
     }
@@ -34,10 +34,34 @@ struct MassageView: View, ActionableView {
         ZStack {
             mainContent
             swipeAnimationOverlay
-            CloseButton { dismiss() }
         }
         .padding()
-        .onAppear(perform: dismissSwipeAnimationAfterDelay)
+        .onAppear {
+            withAnimation(.easeIn.delay(0.2)) {
+                viewState.showCarousel = true
+            }
+            dismissSwipeAnimationAfterDelay()
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    withAnimation(.easeOut(duration: 0.005)) {
+                        viewState.showCarousel = false
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+                        dismiss()
+                    }
+                } label: {
+                    HStack(spacing: 5) {
+                        Image(systemName: "chevron.left")
+                        Text("Back")
+                    }
+                    .foregroundColor(.blue)
+                    .font(.system(size: 17, weight: .regular))
+                }
+            }
+        }
         .sheet(item: $viewState.bookingURL) { booking in
             WebViewContainer(url: booking.url, title: booking.title)
         }
@@ -45,23 +69,26 @@ struct MassageView: View, ActionableView {
 }
 
 private extension MassageView {
-
     var mainContent: some View {
         VStack {
             headerSection
-            carouselSection
+            if viewState.showCarousel {
+                carouselSection
+                    .transition(.opacity)
+            }
             Spacer()
             actionButton
         }
     }
 
     var headerSection: some View {
-        VStack {
+        VStack(spacing: 0) {
             Text(viewState.pageTitle)
                 .font(.sfProDisplayBold(size: 34))
                 .multilineTextAlignment(.center)
                 .foregroundColor(adaptiveTextColor)
-                .padding()
+                .padding(.top, 0)
+                .padding(.bottom, 16)
 
             Text(viewState.pageBio)
                 .font(.sfProBodyTextRegular(size: 16))
