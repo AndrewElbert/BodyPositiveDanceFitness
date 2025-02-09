c0b78f81b0d5a6972c562c65942ab69e883392a4
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-09 16:33:27 -0500

    [34] basic team files

---
+++ BodyPositiveZumba/About/AboutUI/Team/TeamScreen.swift
@@ -0,0 +1,23 @@
+//
+//  TeamScreen.swift
+//  BodyPositiveZumba
+//
+//  Created by Andrew Elbert on 2/9/25.
+//
+
+import SwiftUI
+
+struct TeamScreen: View {
+
+    @ObservedObject private var viewModel: TeamViewModel
+
+    public init(viewModel: TeamViewModel) {
+        self._viewModel = ObservedObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        TeamView(
+            viewState: $viewModel.viewState
+        )
+    }
+}
