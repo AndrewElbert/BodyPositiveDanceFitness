7d5b144599e03b6983721e192ae4a3d01788c40c
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-10 21:49:25 -0500

    [36] new partner files

---
+++ BodyPositiveZumba/About/AboutDomain/Models/Partners/Partner.swift
@@ -0,0 +1,56 @@
+//
+//  Partner.swift
+//  AboutDomain
+//
+//  Created by Andrew Elbert on 2/10/25.
+//
+
+import SwiftUI
+
+enum Partner: String, CaseIterable, Identifiable {
+    
+    case overland = "Overland Honey"
+    case beam = "Beam Skin Studio"
+    case dtx = "Danse Très Xtrême"
+    case llh = "Lady Love Holistics"
+    
+    var id: String { self.rawValue }
+    
+    var url: WebViewURL {
+        switch self {
+        case .overland:
+            return WebViewURL(
+                title: "",
+                url: URL(string: "https://overlandhoney.com")!
+            )
+        case .beam:
+            return WebViewURL(
+                title: "",
+                url: URL(string: "https://www.beamskinstudio.com/")!
+            )
+        case .dtx:
+            return WebViewURL(
+                title: "",
+                url: URL(string: "https://dansetresxtreme.com/")!
+            )
+        case .llh:
+            return WebViewURL(
+                title: "",
+                url: URL(string: "https://ladyloveholistics.com/")!
+            )
+        }
+    }
+    var image: String {
+        switch self {
+            
+        case .overland:
+            return "OverlandLogo"
+        case .beam:
+            return "BeamSkinLogo"
+        case .dtx:
+            return "DtxLogo"
+        case .llh:
+            return "LadyLoveHolisticsLogo"
+        }
+    }
+}
---
+++ BodyPositiveZumba/About/AboutUI/Partners/PartnersViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PartnersViewModel.swift
+//  About
+//
+//  Created by Andrew Elbert on 2/10/25.
+//
+
+import SwiftUI
+
+class PartnersViewModel: ObservableObject {
+
+    @Published var viewState: PartnersViewState = PartnersViewState()
+
+}
