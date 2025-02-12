9ef0208b56e5bcad1b948e6e5d59f94bcab71d2e
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-11 19:42:02 -0500

    [39] updated swipe animation

--- BodyPositiveZumba/Common/CommonUI/SwipeAnimation/SwipeAnimationView.swift
+++ BodyPositiveZumba/Common/CommonUI/SwipeAnimation/SwipeAnimationView.swift
@@ -8,34 +8,54 @@
 import SwiftUI
 
 struct SwipeAnimationView: View {
-
     @Binding var viewState: SwipeAnimationViewState
 
-    public init(
-        viewState: Binding<SwipeAnimationViewState>
-    ) {
-        self._viewState = viewState
+    @State private var offset: CGFloat = 0
+    @State private var opacity: Double = 0.0
+    @State private var swipeDirection: SwipeDirection = .right
+
+    private enum SwipeDirection {
+        case right, left
     }
 
     var body: some View {
-        ZStack {
-            Image(systemName: "hand.point.right.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 40, height: 40)
-                .foregroundColor(Color.gray.opacity(0.8))
-                .offset(x: viewState.isAnimating ? 30 : -30)
-                .opacity(viewState.isAnimating ? 1 : 0.3)
-                .animation(
-                    Animation.easeInOut(duration: 1.8)
-                        .repeatForever(autoreverses: true),
-                    value: viewState.isAnimating
-                )
-                .rotationEffect(.degrees(viewState.isAnimating ? 10 : -10))
+        Image(systemName: "hand.point.up.fill")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 60, height: 60)
+            .foregroundColor(Constants.Colors.neonCyan.opacity(0.9))
+            .offset(x: offset)
+            .opacity(opacity)
+            .frame(width: 100, height: 50)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    animateCycle()
+                }
+            }
+
+    }
+
+    func animateCycle() {
+        let swipeDistance: CGFloat = 111       // how far to move horizontally
+        let fadeInDuration: Double = 0.2        // duration for fading in at the center
+        let swipeDuration: Double = 1.1        // duration for the swipe (and fade-out)
+        let pauseDuration: Double = 0.5         // pause before the next swipe
+
+        offset = 0
+        withAnimation(Animation.linear(duration: fadeInDuration)) {
+            opacity = 1.0
         }
-        .frame(width: 100, height: 50)
-        .onAppear {
-            viewState.isAnimating = true
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + fadeInDuration) {
+            withAnimation(Animation.linear(duration: swipeDuration)) {
+                offset = (swipeDirection == .right) ? swipeDistance : -swipeDistance
+                opacity = 0.0
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + swipeDuration + pauseDuration) {
+                swipeDirection = (swipeDirection == .right) ? .left : .right
+                animateCycle()
+            }
         }
     }
 }
