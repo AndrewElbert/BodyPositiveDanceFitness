ffd4731758526df414c7982716c9b2e7f6c405c0
AndrewElbert <andrew.elbert@alumni.wfu.edu>
2025-02-14 16:16:48 -0500

    [45] swiftlint

--- BodyPositiveZumba/Home/Home/HomeDomain/HomeViewState.swift
+++ BodyPositiveZumba/Home/Home/HomeDomain/HomeViewState.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct HomeViewState {
-    
+
     var showBookClassWebView: Bool = false
     var showJoinWebView: Bool = false
     var currentGreeting: String = ""
-    
+
     var sideDrawerViewState: SideDrawerViewState = SideDrawerViewState()
 }
